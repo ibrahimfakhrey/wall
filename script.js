335eb825316d6e61689608e5bf791f6cb85d3e0b
Ibrahim fakhrey <77210469+ibrahimfakhrey@users.noreply.github.com>
2023-08-15 13:54:31 +0300

    Update script.js
    hh

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ var UserStatus;
 })(UserStatus || (UserStatus = {}));
 var Default;
 (function (Default) {
-    Default["PIN"] = "Daleel@123";
+    Default["PIN"] = "0000";
 })(Default || (Default = {}));
 var WeatherType;
 (function (WeatherType) {
